e5b7cabfac2338d16f0423ef4ffd5a420b7a44ea
Rob Hardy <robhardy777@gmail.com>
2017-10-02 11:45:24 -0400

    2nd commit

---
+++ src/JavaBank.java
@@ -0,0 +1,17 @@
++------------------------------------+
+|       Java Bank ATM Receipt        |
+|       Sunday, September 24, 2017   |
+|       ATM Location # 12            |
+|                                    |
+|                                    |
+|       Account Number    1227       |
+|       Customer: Rob Hardy          |
+|       Transaction Type: Withdrawal |
+|       Transaction Ammount: $12000  |
+|       Account Balance:     $10000  |
+|                                    |
+|                                    |
+|       Thank you for banking        |
+|               with us              |
+|       Have a great day!            |
++------------------------------------+
